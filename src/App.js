514f4c567c30e218e5ae5f139f9071fee919676d
gyeongwoo park <gyeongwoo@bearrobotics.ai>
2024-01-02 18:30:30 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [names, setNames] = useState("");
+  const [data, setData] = useState("");
+  const [result, setResult] = useState([]);
+
+  const handleTransform = () => {
+    const result = names.split("\n").map((name) => {
+      const current = data.split("\n").find((item) => item.includes(name));
+      if (!current) {
+        return {
+          name,
+          result: "",
+        };
+      }
+      try {
+        // find number inside of ()
+        const regex = /\(([^)]+)\)/;
+        const [, result] = current.match(regex);
+
+        return {
+          name,
+          result,
+        };
+      } catch {
+        return {
+          name,
+          result: `${name} 입력 형식이 잘못됨. 수기로 입력 바랍니다. ${current}`,
+        };
+      }
+    });
+
+    setResult(result);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        type="text"
+        placeholder="이름을 순서를 복사 붙혀넣기 하세요"
+        value={names}
+        onChange={(e) => {
+          setNames(e.target.value);
+        }}
+        style={{ height: 500 }}
+      />
+      {result.length > 0 && (
+        <textarea
+          type="text"
+          value={result.map((item) => item.result).join("\n")}
+          style={{ height: 500 }}
+        />
+      )}
+      <textarea
+        type="text"
+        placeholder="데이터를 복사 붙혀넣기 하세요"
+        value={data}
+        onChange={(e) => {
+          setData(e.target.value);
+        }}
+        style={{ height: 500 }}
+      />
+      <button onClick={handleTransform}>변환</button>
     </div>
   );
 }
